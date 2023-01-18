62cc5a800d239b947131806fd3e74fe874343edc
nitish mahawar <nitishmahawar112@gmail.com>
2023-01-18 12:06:22 +0530

    third commit

--- src/components/DarkModeNew.jsx
+++ src/components/DarkModeNew.jsx
@@ -22,11 +22,7 @@ const DarkModeNew = () => {
   ];
 
   const onWindowMatch = () => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
