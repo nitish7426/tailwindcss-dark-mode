344584c38d7e9cfc0af613490fba57bd1e9b63d0
nitish mahawar <nitishmahawar112@gmail.com>
2023-01-18 10:06:01 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,10 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import DarkModeNew from "./components/DarkModeNew";
 
 const App = () => {
-  const [theme, setTheme] = useState(() => {
-    const theme = localStorage.getItem("theme");
-    return theme ? theme : "system";
-  });
-
-  useEffect(() => {
-    if (theme === "system") {
-      localStorage.removeItem("theme");
-    } else {
-      localStorage.theme = theme;
-    }
-  }, [theme]);
-
-  if (
-    theme === "dark" ||
-    (theme === "system" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
-
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", () => {
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
-    });
-
   return (
-    <div className="bg-white h-screen dark:bg-slate-900 flex items-center justify-center">
-      <select
-        className="py-2 px-4 rounded-lg border-2 capitalize font-medium outline-none cursor-pointer dark:text-white dark:border-white/40 bg-transparent dark:bg-slate-800"
-        value={theme}
-        onChange={(e) => setTheme(e.target.value)}
-      >
-        <option
-          className="font-medium capitalize dark:text-white dark:bg-slate-800"
-          value="system"
-        >
-          system
-        </option>
-        <option
-          className="font-medium capitalize dark:text-white dark:bg-slate-800"
-          value="light"
-        >
-          light
-        </option>
-        <option
-          className="font-medium capitalize dark:text-white dark:bg-slate-800"
-          value="dark"
-        >
-          dark
-        </option>
-      </select>
+    <div>
+      <DarkModeNew />
     </div>
   );
 };
---
+++ src/components/DarkModeNew.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import { BiSun, BiMoon, BiDesktop } from "react-icons/bi";
+
+const DarkModeNew = () => {
+  const [theme, setTheme] = useState(() => {
+    let theme = localStorage.getItem("theme");
+    return theme ? theme : "system";
+  });
+  const buttons = [
+    {
+      text: "system",
+      icon: <BiDesktop size={25} />,
+    },
+    {
+      text: "light",
+      icon: <BiSun size={25} />,
+    },
+    {
+      text: "dark",
+      icon: <BiMoon size={25} />,
+    },
+  ];
+
+  const onWindowMatch = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  onWindowMatch();
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", () => {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    });
+
+  useEffect(() => {
+    switch (theme) {
+      case "dark":
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+        break;
+      case "light":
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+        break;
+      default:
+        localStorage.removeItem("theme");
+        break;
+    }
+  }, [theme]);
+
+  return (
+    <div className="min-h-screen w-full py-6 px-4 bg-white dark:bg-slate-900 dark:text-white space-y-5">
+      <div className="w-fit py-2 px-3 rounded-md mx-auto space-x-2 bg-slate-100 dark:bg-slate-800 leading-none">
+        {buttons.map(({ text, icon }) => (
+          <button
+            className={`hover:text-blue-500 ${
+              theme === text && "text-blue-500"
+            }`}
+            key={text}
+            onClick={() => setTheme(text)}
+          >
+            {icon}
+          </button>
+        ))}
+      </div>
+      <h3 className="text-4xl font-bold text-center capitalize">
+        dark mode tailwind css
+      </h3>
+      <p className="text-lg">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+        totam, deleniti aperiam aspernatur explicabo quia perferendis ipsa nihil
+        quam libero voluptas. Quos debitis culpa eum voluptate quisquam impedit
+        ut quod sequi numquam amet nobis molestiae dicta cumque, saepe deserunt
+        voluptas nostrum fugit non? Sapiente corrupti quo consequuntur animi
+        repellat voluptate! Fugit distinctio nemo autem labore nihil dolorum,
+        consectetur vero corporis adipisci rerum voluptates beatae rem soluta
+        tempore sapiente doloremque ab minima suscipit? Exercitationem iusto
+        magnam labore sunt numquam incidunt ratione error fugiat omnis.
+        Distinctio voluptates quod quae ipsa sunt temporibus esse quo, fugiat
+        quis, eveniet repellendus aspernatur, iste nemo dolores?
+      </p>
+    </div>
+  );
+};
+
+export default DarkModeNew;
